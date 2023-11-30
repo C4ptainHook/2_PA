0f0cffb493864807d3748bc0e44dac66f7d9e14f
C4ptainHook <boykostudyinkpi@gmail.com>
2023-11-30 15:58:52 +0200

    Outline of the maze

--- src/Path-Finder/Path-Finder/Program.cs
+++ src/Path-Finder/Path-Finder/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace PathFinder.General
+{
+    internal class Program 
+    {
+        public static void Main(string[] argc)
+        {
+
+        }
+    }
+}
