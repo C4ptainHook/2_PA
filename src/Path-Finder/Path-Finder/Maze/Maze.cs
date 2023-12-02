6281a6d2efdaedabf8aa2bcdf8bc13e54d4314eb
C4ptainHook <boykostudyinkpi@gmail.com>
2023-12-02 17:06:03 +0200

    Basic functionality of maze is done

--- src/Path-Finder/Path-Finder/Maze/Maze.cs
+++ src/Path-Finder/Path-Finder/Maze/Maze.cs
@@ -6,121 +6,118 @@ using System.Threading.Tasks;
 
 namespace Path_Finder.Maze
 {
-    namespace Grid
+    using static Enums;
+
+    public class Maze
     {
-        using static Enums;
-
-        public class Grid
+        private readonly Cell[,] _maze;
+        public Maze(int horizontalCells, int verticalCells)
         {
-            private readonly Cell[,] _grid;
-            public Grid(int horizontalCells, int verticalCells)
+            _maze = new Cell[horizontalCells, verticalCells];
+            for (var x = 0; x < _maze.GetLength(0); x++)
             {
-                _grid = new Cell[horizontalCells, verticalCells];
-                for (var x = 0; x < _grid.GetLength(0); x++)
+                for (var y = 0; y < _maze.GetLength(1); y++)
                 {
-                    for (var y = 0; y < _grid.GetLength(1); y++)
-                    {
-                        SetCell(x, y, CellType.Empty);
-                    }
+                    SetCell(x, y, CellType.Empty);
                 }
-
-                SetStartAndEnd();
             }
 
-            public void Randomize()
-            {
-                Randomize((int)DateTime.Now.Ticks);
-            }
+            SetStartAndEnd();
+        }
 
-            public void Randomize(int seed)
-            {
-                var rand = new Random(seed);
+        public void Randomize()
+        {
+            Randomize((int)DateTime.Now.Ticks);
+        }
 
-                // Iterate through the whole grid
-                for (var x = 0; x < _grid.GetLength(0); x++)
+        public void Randomize(int seed)
+        {
+            var rand = new Random(seed);
+
+            // Iterate through the whole grid
+            for (var x = 0; x < _maze.GetLength(0); x++)
+            {
+                for (var y = 0; y < _maze.GetLength(1); y++)
                 {
-                    for (var y = 0; y < _grid.GetLength(1); y++)
-                    {
-                        // Make each cell either solid or empty at random
-                        _grid[x, y].Type = rand.Next(0, 10) > 5 ? CellType.Solid : CellType.Empty;
-                        if (_grid[x, y].Type != CellType.Empty) continue;
+                    // Make each cell either solid or empty at random
+                    _maze[x, y].Type = rand.Next(0, 10) > 5 ? CellType.Solid : CellType.Empty;
+                    if (_maze[x, y].Type != CellType.Empty) continue;
 
-                        // If it's empty, randomly give the path a weight
-                        var weightSpread = rand.Next(0, 10);
-                        if (weightSpread > 8)
-                            _grid[x, y].Weight = 3;
-                        else if (weightSpread > 6)
-                            _grid[x, y].Weight = 2;
-                        else
-                            _grid[x, y].Weight = 1;
-                    }
+                    // If it's empty, randomly give the path a weight
+                    var weightSpread = rand.Next(0, 10);
+                    if (weightSpread > 8)
+                        _maze[x, y].Weight = 3;
+                    else if (weightSpread > 6)
+                        _maze[x, y].Weight = 2;
+                    else
+                        _maze[x, y].Weight = 1;
                 }
-                SetStartAndEnd();
             }
+            SetStartAndEnd();
+        }
 
-            public Cell GetCell(int x, int y)
+        public Cell GetCell(int x, int y)
+        {
+            if (x > _maze.GetLength(0) - 1 || x < 0 || y > _maze.GetLength(1) - 1 || y < 0) return new Cell { Coord = new Coord(-1, -1), Type = CellType.Invalid };
+
+            return _maze[x, y];
+        }
+
+        public Cell GetStart()
+        {
+            return _maze.Cast<Cell>().FirstOrDefault(cell => cell.Type == CellType.A);
+        }
+
+        public Cell GetEnd()
+        {
+            return _maze.Cast<Cell>().FirstOrDefault(cell => cell.Type == CellType.B);
+        }
+
+        public void SetCell(int x, int y, CellType type)
+        {
+            _maze[x, y] = new Cell
             {
-                if (x > _grid.GetLength(0) - 1 || x < 0 || y > _grid.GetLength(1) - 1 || y < 0) return new Cell { Coord = new Coord(-1, -1), Type = CellType.Invalid };
+                Coord = new Coord(x, y),
+                Type = type,
+                Weight = GetCell(x, y)?.Weight ?? 0
+            };
 
-                return _grid[x, y];
-            }
+            SetStartAndEnd();
+        }
 
-            public Cell GetStart()
+        public void SetCell(Coord coord, CellType type)
+        {
+            SetCell(coord.X, coord.Y, type);
+        }
+
+        public int GetCountOfType(CellType type)
+        {
+            var total = 0;
+            foreach (var cell in _maze)
             {
-                return _grid.Cast<Cell>().FirstOrDefault(cell => cell.Type == CellType.A);
+                total += cell.Type == type ? 1 : 0;
             }
 
-            public Cell GetEnd()
+            return total;
+        }
+
+        public int GetTraversableCells()
+        {
+            return GetCountOfType(CellType.Open) + GetCountOfType(CellType.A) + GetCountOfType(CellType.B);
+        }
+
+        private void SetStartAndEnd()
+        {
+            _maze[0, 0] = new Cell
             {
-                return _grid.Cast<Cell>().FirstOrDefault(cell => cell.Type == CellType.B);
-            }
-
-            public void SetCell(int x, int y, CellType type)
+                Coord = new Coord(0, 0),
+                Type = CellType.A
+            };
+            _maze[_maze.GetLength(0) - 1, _maze.GetLength(1) - 1] = new Cell
             {
-                _grid[x, y] = new Cell
-                {
-                    Coord = new Coord(x, y),
-                    Type = type,
-                    Weight = GetCell(x, y)?.Weight ?? 0
-                };
-
-                SetStartAndEnd();
-            }
-
-            public void SetCell(Coord coord, CellType type)
-            {
-                SetCell(coord.X, coord.Y, type);
-            }
-
-            public int GetCountOfType(CellType type)
-            {
-                var total = 0;
-                foreach (var cell in _grid)
-                {
-                    total += cell.Type == type ? 1 : 0;
-                }
-
-                return total;
-            }
-
-            public int GetTraversableCells()
-            {
-                return GetCountOfType(CellType.Open) + GetCountOfType(CellType.A) + GetCountOfType(CellType.B);
-            }
-
-            private void SetStartAndEnd()
-            {
-                _grid[0, 0] = new Cell
-                {
-                    Coord = new Coord(0, 0),
-                    Type = CellType.A
-                };
-                _grid[_grid.GetLength(0) - 1, _grid.GetLength(1) - 1] = new Cell
-                {
-                    Coord = new Coord(_grid.GetLength(0) - 1, _grid.GetLength(1) - 1),
-                    Type = CellType.B
-                };
-            }
+                Coord = new Coord(_maze.GetLength(0) - 1, _maze.GetLength(1) - 1),
+                Type = CellType.B
+            };
         }
     }
 }
