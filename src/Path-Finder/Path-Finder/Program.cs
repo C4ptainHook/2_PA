31dc314aa6dd60e7e44f1d30554aec2842dc8296
C4ptainHook <boykostudyinkpi@gmail.com>
2023-12-02 17:41:58 +0200

    Get code from LDFS branch. Need feature to store and get Maze from file

--- src/Path-Finder/Path-Finder/Program.cs
+++ src/Path-Finder/Path-Finder/Program.cs
@@ -1,11 +1,51 @@
 ﻿
-namespace PathFinder.General
+namespace Path_Finder
 {
-    internal class Program 
-    {
-        public static void Main(string[] argc)
-        {
+    using System;
+    using System.Linq;
+    using Path_Finder.Algorithms;
+    using Path_Finder.Maze;
+    using static System.Net.Mime.MediaTypeNames;
 
+    public partial class Main
+    {
+        private AlgorithmBase[] _algorithms;
+        private int _currentAlgorithm;
+        private readonly System.Timers.Timer _pathTimer;
+        private const int Delay = 5;
+
+        public Main()
+        {
+            InitialiseMaze();
+        }
+
+        /// <summary>
+        /// Set up a maze and initialise the algorithm variables
+        /// </summary>
+        private void InitialiseMaze()
+        {
+            _pathTimer.Stop();
+
+            // Generate mazes until one if made that has a valid path between A and B
+            var workingSeed = 0;
+            while (workingSeed == 0)
+                workingSeed = FindWorkingSeed();
+        }
+
+        /// <summary>
+        /// Generate mazes until one is found that has a valid path between A and B
+        /// </summary>
+        /// <returns>The seed of the valid maze</returns>
+        private int FindWorkingSeed()
+        {
+            var testPathFinder = new DFS(testMazeDrawer.Grid);
+            var progress = testPathFinder.GetPathTick();
+            while (progress.PathPossible && !progress.PathFound)
+            {
+                progress = testPathFinder.GetPathTick();
+            }
+
+            return progress.PathFound ? testMazeDrawer.Seed : 0;
         }
     }
 }
